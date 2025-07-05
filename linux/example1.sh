5af90649645a143a2827f6e0b94bba5f9e724816
Prajwal Vijay Ankushrao <prajwal11@Prajwals-MacBook-Air.local>
2025-07-05 20:55:48 +0530

    PA | Shell Script example

--- linux/example1.sh
+++ linux/example1.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 #########################
-#
-#
-#
-#
-#
-########################
+# Script to print numbers from 1 to 100 that are divisible by 3 or 5,
+# but NOT divisible by 15
+#########################
 
-
+# Loop through numbers 1 to 100
 for i in {1..100}; do 
-if ([ `expr $i % 3` == 0 ] || [ `expr $i % 5` == 0 ]) && [ `expr $i % 15` != 0 ];
-then 
-	echo $i
-fi;
-done 
+    # Check if divisible by 3 OR 5, AND NOT divisible by 15
+    if ([ `expr $i % 3` == 0 ] || [ `expr $i % 5` == 0 ]) && [ `expr $i % 15` != 0 ]; then 
+        echo $i  # Print the number
+    fi
+done
